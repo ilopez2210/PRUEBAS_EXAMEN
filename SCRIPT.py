8a1d2e3bc58dbe19d383894bab6a57d9048bdf42
Isaac Lopez <ilopezgrande2210@ieszaidinvergeles.org>
2023-02-21 14:10:33 +0100

    PRUEBA

---
+++ SCRIPT.py
@@ -0,0 +1,16 @@
+import sys 
+from bs4 import BeautifulSoup
+import requests
+
+if len(sys.argv) != 2:
+    print("El programa necesita 2 argumentos para funcionar")
+
+else:
+    canal=sys.argv[1]
+    result = requests.get(canal,cookies={'CONSENT':'YES+1'})
+    soup = BeautifulSoup(result.text, 'html.parser')
+    with open("codigo.txt", "w", encoding="utf-8") as texto:
+        texto.write(result.text)
+    print(soup.title.text)
+    open('codigo.txt', 'r').read()
+
